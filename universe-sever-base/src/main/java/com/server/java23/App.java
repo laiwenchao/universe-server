74b4df94a28b3761f8556d449c1b14b2d9558029
赖文超 <laiwenchao02@meituan.com>
2019-06-17 21:42:42 +0800

    java设计模式

---
+++ universe-sever-base/src/main/java/com/server/java23/App.java
@@ -0,0 +1,9 @@
+package com.server.java23;
+
+/**
+ * @Description:
+ * @Author: laiwenchao02
+ * @Date: 2019/6/17 下午9:41
+ */
+public class App {
+}
---
+++ universe-sever-base/src/main/java/com/server/java23/factory/SimpleFactory.java
@@ -0,0 +1,9 @@
+package com.server.java23.factory;
+
+/**
+ * @Description:
+ * @Author: laiwenchao02
+ * @Date: 2019/6/17 下午9:42
+ */
+public class SimpleFactory {
+}
