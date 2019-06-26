c669f1e32d58c3680bc22ca762f16c4593a00bc9
laiwenchao02 <15727338538@163.com>
2019-06-26 18:29:51 +0800

    工厂方法

--- universe-sever-base/src/main/java/com/server/java23/factory/abstraction/AbstractFactory.java
+++ universe-sever-base/src/main/java/com/server/java23/factory/abstraction/AbstractFactory.java
@@ -4,7 +4,8 @@ import com.server.java23.factory.abstraction.domain.Fruit;
 import com.server.java23.factory.abstraction.domain.Vegetables;
 
 /**
- * @Description: 抽象工厂，创建水果和蔬菜
+ * @Description: 抽象工厂，可以生产多个产品；例如：创建水果和蔬菜；但是每个产品都是一系列的，比如都是北方的；
+ *               工厂方法只能生产一个产品例如都是水果；
  * @Author: laiwenchao02
  * @Date: 2019/6/18 下午9:06
  */
--- universe-sever-base/src/main/java/com/server/java23/factory/general/Creator.java
+++ universe-sever-base/src/main/java/com/server/java23/factory/general/Creator.java
@@ -3,7 +3,9 @@ package com.server.java23.factory.general;
 import com.server.java23.factory.Fruit;
 
 /**
- * @Description:
+ * @Description: 简单工厂抽象类，一个子类对应一个工厂（开闭原则），对简单工厂进行了解耦
+ * 缺点：要增加产品类时也要相应地增加工厂类
+ *
  * @Author: laiwenchao02
  * @Date: 2019/6/18 下午8:14
  */
