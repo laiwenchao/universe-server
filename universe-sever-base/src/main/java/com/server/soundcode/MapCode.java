5895bf4e9af82981847b7e6efea11cf0814a0ea2
laiwenchao02 <15727338538@163.com>
2019-07-02 11:12:08 +0800

    map源码分析

--- universe-sever-base/src/main/java/com/server/soundcode/MapCode.java
+++ universe-sever-base/src/main/java/com/server/soundcode/MapCode.java
@@ -3,6 +3,11 @@ package com.server.soundcode;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * @Author laiwenchao
+ * @Description hashmap源码解读 https://www.cnblogs.com/xiaoxi/p/7233201.html
+ * @Date 2019-07-01 17:42
+ **/
 public class MapCode {
     public static void main(String[] args) {
         Map<String, String> map = new HashMap<String, String>();
