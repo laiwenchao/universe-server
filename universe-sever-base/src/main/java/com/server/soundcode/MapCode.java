b5beef498485216d458e87e21e4e59d219b203b9
laiwenchao02 <15727338538@163.com>
2019-07-02 16:52:59 +0800

    hashmap

--- universe-sever-base/src/main/java/com/server/soundcode/MapCode.java
+++ universe-sever-base/src/main/java/com/server/soundcode/MapCode.java
@@ -6,10 +6,11 @@ import java.util.Map;
 /**
  * @Author laiwenchao
  * @Description hashmap源码解读 https://www.cnblogs.com/xiaoxi/p/7233201.html
+ * 通过 (数组桶长度-1) & hash 得到元素存放的位置
  * @Date 2019-07-01 17:42
  **/
 public class MapCode {
     public static void main(String[] args) {
-        Map<String, String> map = new HashMap<String, String>();
+        Map<String, Integer> map = new HashMap<String, Integer>();
     }
 }
