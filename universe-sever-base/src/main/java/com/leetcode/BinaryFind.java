59030e830fb721d69edb7c4da98ce866bee01c64
laiwenchao02 <15727338538@163.com>
2019-09-16 22:48:45 +0800

    二分查找

--- universe-sever-base/src/main/java/com/leetcode/BinaryFind.java
+++ universe-sever-base/src/main/java/com/leetcode/BinaryFind.java
@@ -8,18 +8,18 @@ package com.leetcode;
  */
 public class BinaryFind {
     public static void main(String[] args) {
-        int[] nums = {1, 3, 4, 7, 11, 16, 33, 43, 65, 77, 88, 90, 99, 110};
-        int result = binaryFind(0, nums.length - 1, 65, nums);
+        int[] nums = {1, 3, 3, 3, 4, 7, 11, 16, 33, 43, 65, 77, 88, 90, 99, 110};
+        int result = binaryFind(0, nums.length - 1, 3, nums);
         System.out.println(result);
 
     }
 
     /**
+     * @return int
      * @Author laiwenchao
      * @Description 二分查找
      * @Date 2019-09-15 19:46
      * @Param [start, end, target, nums]
-     * @return int
      **/
     public static int binaryFind(int start, int end, int target, int[] nums) {
 
@@ -27,9 +27,9 @@ public class BinaryFind {
         if (target == nums[mid]) {
             return mid;
         } else if (target > nums[mid]) {
-            return binaryFind(mid, end, target, nums);
+            return binaryFind(mid+1, end, target, nums);
         } else {
-            return binaryFind(0, mid, target, nums);
+            return binaryFind(0, mid+1, target, nums);
         }
 
     }
